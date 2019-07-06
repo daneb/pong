196296003baa4837c43eb40dd45185dfc4fece94
daneb <enteritos@gmail.com>
2019-07-06 21:39:49 +0100

    Reverted code back to es6 with improved understanding

--- game.js
+++ game.js
@@ -1,25 +1,29 @@
 // The game engine
 
 class Game {
+
   constructor(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext("2d");
     this.width = canvas.width;
     this.height = canvas.height;
     this.keyPressed = {};
-    this.keys = {
+  }
+
+  static keys(idOfKeyPressed) {
+    var keys = {
       32: 'space',
       37: 'left',
       38: 'up',
       39: 'right',
       40: 'down'
     };
-
+    return keys[idOfKeyPressed];
   }
 
-  getKeysPressed(e) {
+  getKeysPressed = (e) => {
     // Convert Key Code to Key Name
-    let keyName = this.keys[e.which];
+    let keyName = Game.keys(e.which);
 
     if (keyName) {
       this.keyPressed[keyName] = e.type === 'keydown';  
@@ -27,31 +31,29 @@ class Game {
     }
   }
 
-  start() {
-    var self = this;
+  start = () => {
     let fps = 60;
     let interval = 1000 / fps;
 
-    self.canvas.addEventListener('keydown', self.getKeysPressed, false);
-    self.canvas.addEventListener('keyup', self.getKeysPressed, false);
+    this.canvas.addEventListener('keydown', this.getKeysPressed, false);
+    this.canvas.addEventListener('keyup', this.getKeysPressed, false);
 
-    setInterval(function() {
-      self.update();
-      self.draw();
+    setInterval(() => {
+      this.update();
+      this.draw();
     }, interval);
   }
 
-  update() {
+  update = () => {
     this.entities.forEach(entity => {
       if (entity.update) entity.update();
     });
   }
 
-  draw() {
-    var self = this;
-
+  draw = () =>  {
     this.entities.forEach(entity => {
-      if (entity.draw) entity.draw(self.context);
+      if (entity.draw) entity.draw(this.context);
     })
   }
 }
+
