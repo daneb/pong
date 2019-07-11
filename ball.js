09b251b45b42c6955b732bfa0351ad795491717e
daneb <enteritos@gmail.com>
2019-07-11 14:37:11 +0100

    Complete

--- ball.js
+++ ball.js
@@ -1,19 +1,51 @@
 class Ball extends Entity {
+
     constructor() {
         super();
         this.width = 20;
         this.height = 20;  
 
+        this.reset();
+    }
+
+    reset() {
         this.x = game.width / 2 - this.width;
         this.y = game.height / 2 - this.height;
+
+        let min = 5, max = 5;
         
-        this.yVelocity = 10;
+        this.yVelocity = Math.floor(Math.random() * (max - min +1) + min);
+        this.xVelocity = Math.random() > 0.5 ? 5 : -5;
     }
 
     update() {
+
         super.update();
         if(this.y > game.height - this.height || this.y < 0) {
             this.yVelocity *= -1;
+        }
+
+        if(this.x > game.width) {
+            game.player.score += 1;
+            this.reset();
+        }
+
+        if(this.x < 0) {
+            game.bot.score += 1;
+            this.reset();
+        }
+
+        if (this,this.intersect(game.bot)) {
+            var hitter = game.bot;
+        } else if (this.intersect(game.player)) {
+            var hitter = game.player;
+        }
+
+        if(hitter) {
+            this.xVelocity *= -1.1;
+            this.yVelocity *= -1.1;
+
+            this.yVelocity += hitter.yVelocity / 4;
         }
     }
 }
--- pong.js
+++ pong.js
@@ -2,6 +2,11 @@ class Background {
     draw(context) {
         context.fillStyle = '#000';
         context.fillRect(0, 0, game.width, game.width);
+
+        context.fillStyle = '#fff';
+        context.font = "40px monospace";
+        context.fillText(game.player.score, game.width * 3/ 8, 50);
+        context.fillText(game.bot.score, game.width * 5 / 8, 50);
     }
 }
 
@@ -10,8 +15,9 @@ var canvas = document.getElementById('canvas'),
 
 game.entities = [
     new Background(),
-    new Ball(),
-    new Player()
+    game.ball = new Ball(),
+    game.player = new Player(),
+    game.bot = new Bot()
 ];
 
 game.start();
