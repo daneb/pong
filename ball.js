332eee5f0fb34028ea1f2c2bb990abe7cd31cbca
daneb <enteritos@gmail.com>
2019-07-11 08:03:52 +0100

    Refactor the js scope

--- ball.js
+++ ball.js
@@ -2,15 +2,17 @@ class Ball extends Entity {
     constructor() {
         super();
         this.width = 20;
-        this.height = 20;    
+        this.height = 20;  
+
         this.x = game.width / 2 - this.width;
         this.y = game.height / 2 - this.height;
         
         this.yVelocity = 10;
     }
 
-    update = () => {
-        if(this.y > game.height) {
+    update() {
+        super.update();
+        if(this.y > game.height - this.height || this.y < 0) {
             this.yVelocity *= -1;
         }
     }
